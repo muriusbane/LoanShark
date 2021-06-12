93105a06a04c1e1183e0f22f3d33627970256749
Renellie Alfred Maglasang <technurvuz@gmail.com>
2021-06-12 14:03:42 +0800

    Add memberList on Member Index

--- LoanShark/Controllers/MemberController.cs
+++ LoanShark/Controllers/MemberController.cs
@@ -1,16 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LoanShark.Data;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LoanShark.ViewModel;
 
 namespace LoanShark.Controllers
 {
     public class MemberController : Controller
     {
+
+        private readonly AppDbContext _db;
+        public MemberController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var repo = new MemberRepository(_db);
+            var memberList = repo.GetMembers();
+            return View(memberList);
         }
     }
 }
