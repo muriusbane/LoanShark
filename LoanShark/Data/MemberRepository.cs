fdba7a42ef7ffa77bec65337a485db0ff1e12c89
Renellie Alfred Maglasang <technurvuz@gmail.com>
2021-06-12 13:10:25 +0800

    Add member repository

---
+++ LoanShark/Data/MemberRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoanShark.Models;
+using LoanShark.ViewModel;
+
+namespace LoanShark.Data
+{
+    public class MemberRepository
+    {
+    }
+}
