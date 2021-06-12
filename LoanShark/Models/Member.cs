fb0836eff3c119936bf63130f72abe5415378707
Renellie Alfred Maglasang <technurvuz@gmail.com>
2021-06-12 12:39:09 +0800

    Add int pkey MemberId

--- LoanShark/Models/Member.cs
+++ LoanShark/Models/Member.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace LoanShark.Models
 {
     public class Member
     {
+        [Key]
+        public int MemberId { get; set; }
     }
 }
