ef6c1c30231d7018b907a26a2d64d261354ef237
Renellie Alfred Maglasang <technurvuz@gmail.com>
2021-06-12 13:37:51 +0800

    Add AppDbContext dependency to Member repository

--- LoanShark/Data/MemberRepository.cs
+++ LoanShark/Data/MemberRepository.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data;
 using System.Threading.Tasks;
 using LoanShark.Models;
 using LoanShark.ViewModel;
+using Microsoft.EntityFrameworkCore;
 
 namespace LoanShark.Data
 {
     public class MemberRepository
     {
+        private readonly AppDbContext _db;
+        public MemberRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
     }
 }
