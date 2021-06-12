5cbb54d4193d580002b300ce23c2cd5f91765aa9
Renellie Alfred Maglasang <technurvuz@gmail.com>
2021-06-12 13:36:53 +0800

    Change MemberId from int to Guid

--- LoanShark/Models/Member.cs
+++ LoanShark/Models/Member.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LoanShark.Models
 {
     public class Member
     {
         [Key]
-        public int MemberId { get; set; }
+        [Column(Order = 1)]
+        public Guid MemberId { get; set; }
         [Required]
         public string FirstName { get; set; }
         [Required]
