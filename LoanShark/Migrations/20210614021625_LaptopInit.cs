620f3b9b38a5a9aa8380384a5774658b7bc35093
Renellie Alfred Maglasang <technurvuz@gmail.com>
2021-06-14 14:41:14 +0800

    DB Migration for Laptop

---
+++ LoanShark/Migrations/20210614021625_LaptopInit.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LoanShark.Migrations
+{
+    public partial class LaptopInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
